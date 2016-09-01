0227d4b3edb133304f6d36747952ce1226fe794b
Joao Uchoa <joaopedrouchoa@hotmail.com>
2016-09-01 13:29:16 -0300

    more char name

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -6,7 +6,7 @@ class Candidate < ApplicationRecord
   validates :latitude , numericality: { greater_than_or_equal_to:  -90, less_than_or_equal_to:  90 }
   validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
 
-  validates :name, length: { in: 2..20 }
+  validates :name, length: { in: 2..30 }
   validates :description, length: { in: 0..140 }
 
   #TODO
