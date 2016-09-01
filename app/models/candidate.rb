362a39b627806839036741b873d8057fdf61f507
Joao Uchoa <joaopedrouchoa@hotmail.com>
2016-09-01 13:32:24 -0300

    removed description requiriment

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,7 +1,7 @@
 class Candidate < ApplicationRecord
   mount_uploader :image, PlaceUploader
 
-  validates :name, :description, :image, :latitude, :longitude, :user_id, presence: true
+  validates :name, :image, :latitude, :longitude, :user_id, presence: true
 
   validates :latitude , numericality: { greater_than_or_equal_to:  -90, less_than_or_equal_to:  90 }
   validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
